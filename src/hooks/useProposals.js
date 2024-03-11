382878366ddf0f782c723311bf7ac94cb782bd8e
unknown <adefokunadeoluwaisrael@gmail.com>
2024-03-11 12:13:30 +0100

    update code

--- src/hooks/useProposals.js
+++ src/hooks/useProposals.js
@@ -2,32 +2,37 @@ import { useEffect, useState } from "react";
 import { getProposalsContract } from "../constants/contracts";
 import { readOnlyProvider } from "../constants/provider";
 import { decodeBytes32String } from "ethers";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const useProposals = () => {
-  const [proposals, setProposal] = useState({
+  const [proposals, setProposals] = useState({
     loading: true,
     data: [],
   });
 
   useEffect(() => {
-    const contract = getProposalsContract(readOnlyProvider);
-
-    contract
-      .getAllProposals()
-      .then((res) => {
+    const fetchData = async () => {
+      try {
+        const contract = getProposalsContract(readOnlyProvider);
+        const res = await contract.getAllProposals();
         const converted = res.map((item) => ({
           name: decodeBytes32String(item.name),
           voteCount: item.voteCount,
         }));
-        setProposal({
+        setProposals({
           loading: false,
           data: converted,
         });
-      })
-      .catch((err) => {
-        console.error("error fetching proposals: ", err);
-        setProposal((prev) => ({ ...prev, loading: false }));
-      });
+        // toast.success("Proposals fetched successfully");
+      } catch (error) {
+        console.error("Error fetching proposals: ", error);
+        toast.error("Failed to fetch proposals");
+        setProposals((prev) => ({ ...prev, loading: false }));
+      }
+    };
+
+    fetchData();
   }, []);
 
   return proposals;
